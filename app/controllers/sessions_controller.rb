7f9697e612e0394a4b7788d96ea50aeb2b78dead
zhangruihailang <zhangruihailang@163.com>
2015-07-12 06:24:59 +0000

    edited user-login-test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,27 @@ class SessionsController < ApplicationController
   def new
   end
   
+  # def create
+  #   user = User.find_by(email:params[:session][:email].downcase)
+  #   if user && user.authenticate(params[:session][:password])
+  #     #登陆用户
+  #     log_in user
+  #     #remember user
+  #     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  #     redirect_to user
+  #   else
+  #     flash.now[:danger] = "Invalid email/password combination!"
+  #     render 'new'
+  #   end
+  # end
   def create
-    user = User.find_by(email:params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    @user = User.find_by(email:params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
       #登陆用户
-      log_in user
+      log_in @user
       #remember user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = "Invalid email/password combination!"
       render 'new'
