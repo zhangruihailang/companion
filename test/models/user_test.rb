6d0b43c53a2221eaa6f2a7a0a28171d50898414d
zhangruihailang <zhangruihailang@163.com>
2015-07-21 15:21:23 +0000

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,6 +67,17 @@ class UserTest < ActiveSupport::TestCase
     @user.microposts.create!(content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
     @user.destroy
+    end
   end
-end
+  
+  test "should follow and unfollow a user" do
+    michael = users(:zhangrui)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
 end
