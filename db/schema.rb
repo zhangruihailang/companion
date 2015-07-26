37a6d8cf98c693ea75062626b20ac938bd55696c
zhangruihailang <zhangruihailang@163.com>
2015-07-26 07:39:57 +0000

    add test file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724051841) do
+ActiveRecord::Schema.define(version: 20150724030825) do
 
 # Could not dump table "microposts" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
