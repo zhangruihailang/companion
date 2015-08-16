e0d439027e137d13dafee74241f3e326752a89d8
zhangruihailang <zhangruihailang@163.com>
2015-08-16 14:00:57 +0000

    使用ruby实现ucpass短信验证测试2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -195,13 +195,17 @@ class UsersController < ApplicationController
     
     
     sig = Digest::MD5.hexdigest(("439c50e3ccf174139c13def5a00be034"+"49ee5da5489b144012728f719ae506a7"+DateTime.parse(Time.now.to_s).strftime('%Y%m%d%H%M%S').to_s)).upcase
-    url = URI.parse("https://api.ucpaas.com/2014-06-30/Accounts/439c50e3ccf174139c13def5a00be034/Messages/templateSMS.xml?sig=#{sig}")
-    path = "/2014-06-30/Accounts/439c50e3ccf174139c13def5a00be034/Messages/templateSMS.xml?sig=#{sig}"
+    # url = URI.parse("https://api.ucpaas.com/2014-06-30/Accounts/439c50e3ccf174139c13def5a00be034/Messages/templateSMS.xml?sig=#{sig}")
+    # path = "/2014-06-30/Accounts/439c50e3ccf174139c13def5a00be034/Messages/templateSMS.xml?sig=#{sig}"
+    url = URI.parse("https://api.ucpaas.com/2014-06-30/Accounts/439c50e3ccf174139c13def5a00be034/Messages/templateSMS?sig=#{sig}")
+    path = "/2014-06-30/Accounts/439c50e3ccf174139c13def5a00be034/Messages/templateSMS?sig=#{sig}"
     https = Net::HTTP.new(url.host,url.port)
     https.use_ssl = true    
     authorization = Base64.strict_encode64("439c50e3ccf174139c13def5a00be034"+":"+DateTime.parse(Time.now.to_s).strftime('%Y%m%d%H%M%S').to_s)
-    req = Net::HTTP::Post.new(path,{'Content-Type' => 'application/xml;charset=utf-8','Accept' => 'application/xml','Authorization' => "#{authorization}"})
-    data =	"<?xml version='1.0' encoding='utf-8'?><templateSMS><appId>ae84fc15535a411093ff63b830969509</appId><templateId>4892</templateId><to>#{params[:mobile]}</to><param>#{@smscode}</param></templateSMS>"
+    req = Net::HTTP::Post.new(path,{'Content-Type' => 'application/json;charset=utf-8','Accept' => 'application/json','Authorization' => "#{authorization}"})
+    #data =	"<?xml version='1.0' encoding='utf-8'?><templateSMS><appId>ae84fc15535a411093ff63b830969509</appId><templateId>4892</templateId><to>#{params[:mobile]}</to><param>#{@smscode}</param></templateSMS>"
+    data = "{\"templateSMS\" : {\"appId\": \"ae84fc15535a411093ff63b830969509\",\"param\": \"#{@smscode}<\",\"templateId\": \"4892\",\"to\": \"#{params[:mobile]}\"}}"
+
     req.body = data
     res = https.request(req)
     p "------------------------receive----#{res.body}-----------------------------------------------"
