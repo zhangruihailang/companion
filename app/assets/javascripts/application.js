e0bbd4c5e5179e9883d567c7988a19131b9c1c88
zhangruihailang <zhangruihailang@163.com>
2015-09-28 13:08:16 +0000

    修改js失效问题1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
- //= require jquery_ujs
+
+
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
 //= require kindeditor
+//= require turbolinks
 
